d2eb2df66c324b7b67a07261ea7657d39b9e7d13
vucben99-shiwaforce <155745849+vucben99-shiwaforce@users.noreply.github.com>
2024-05-10 12:11:11 +0200

    added doc content replacer

--- console-log.js
+++ console-log.js
@@ -1,1 +1,4 @@
 console.log('OUTSIDE SCRIPT')
+
+const documents = document.getElementsByClassName('documents');
+documents.forEach((doc) => {doc.innerHTML = `<span>KÓDDOBOZ MEGFUT A DOKURA</span>`});
